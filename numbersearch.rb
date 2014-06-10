a3bc27221008b11881dff0e292ff513e2ed225dd
Germain Boue <boue.germain@gmail.com>
2014-06-10 14:57:25 -0700

    Pseudocode

---
+++ numbersearch.rb
@@ -0,0 +1,9 @@
+#take the str parameter, search for all the numbers in the string,
+#add them together, then return that final number.
+#input = "a string"
+#output = "an integer that is the sum of all numbers in string"
+#EDGECASE: if numbers are at neighboring index then they form one number
+
+def NumberAddition(str)
+
+end
