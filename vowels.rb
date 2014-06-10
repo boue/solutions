5a940d3fae84a4640c8d7132f1f8cb5a280bac0c
Germain Boue <boue.germain@gmail.com>
2014-06-10 14:46:13 -0700

    Takes into account edge case where vowel could be a capital letter

--- vowels.rb
+++ vowels.rb
@@ -2,6 +2,10 @@
 #output = an integer which represents the number of vowels present in the string 
 
 def VowelCount(str)
-
-  
+	str.scan(/[aeiouAEIOU]/).size
 end
+
+p VowelCount("hi there") == 3
+p VowelCount("hI thEre") == 3
+p VowelCount("hold") == 1
+p VowelCount("h") == 0
