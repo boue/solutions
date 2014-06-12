77e59bfe7605d54dfbd05db936b02685ddadc0e7
Germain Boue <boue.germain@gmail.com>
2014-06-11 23:08:10 -0700

    Initialize the method and new hash within

--- list_pairs.rb
+++ list_pairs.rb
@@ -3,3 +3,7 @@
 #output = nested array that accounts for the word / count as a pair 
 #The initial idea was to use an array but it didn't feel like the right data structure, 
 #we should use a hash in this case which makes things easier. 
+
+def word_count(words)
+	index = Hash.new(0)
+end
