d58c0474d5e15a86aef7552d4a7a5a6f30f923e0
Germain Boue <boue.germain@gmail.com>
2014-06-11 23:07:29 -0700

    Notes on pseudcode added

--- list_pairs.rb
+++ list_pairs.rb
@@ -1,5 +1,5 @@
 #Problem: Given a list of words, return a list of [word, count] pair, order by count.
 #input = array of strings 
 #output = nested array that accounts for the word / count as a pair 
-
-
+#The initial idea was to use an array but it didn't feel like the right data structure, 
+#we should use a hash in this case which makes things easier. 
