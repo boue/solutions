5944bd13b4a1c87889acce21f207f074e607772f
Germain Boue <boue.germain@gmail.com>
2014-06-11 23:06:00 -0700

    Initial pseudocode + problem

---
+++ list_pairs.rb
@@ -0,0 +1,5 @@
+#Problem: Given a list of words, return a list of [word, count] pair, order by count.
+#input = array of strings 
+#output = nested array that accounts for the word / count as a pair 
+
+
