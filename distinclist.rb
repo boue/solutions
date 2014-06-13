e3372702d96b5503af705546afb58f8ea7f24c1c
Germain Boue <boue.germain@gmail.com>
2014-06-12 17:59:07 -0700

    Simply by created a seperate array with uniq elements you can compare how many times integers were repeated.

---
+++ distinclist.rb
@@ -0,0 +1,11 @@
+#Problem: Using the Ruby language, have the function DistinctList(arr) take the array of numbers stored in arr and determine the total number of duplicate entries. For example if the input is [1, 2, 2, 2, 3] then your program should output 2 because there are two duplicates of one of the elements.
+
+def DistinctList(arr)
+
+	#step1: make an array that is the uniq version
+	unique_arr = arr.uniq
+	#step2: compare length 
+	arr.length - unique_arr.length
+end
+
+p DistinctList([1,2,2,2,3])
