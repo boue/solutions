88386a690f7419bc09548777f156e67cf28e053b
Germain Boue <boue.germain@gmail.com>
2014-06-15 17:23:30 -0700

    Pseudocode

---
+++ dash.rb
@@ -0,0 +1,14 @@
+#Input: integer 
+#output: a string with dashes
+
+def DashInsert(num)
+
+  # step1: convert the integer to a string
+  # step2: determine whether a number is odd or not and grab index 
+  # step3: insert a dash after it 
+
+  num_arr = num.to_s
+  # code goes here
+  return num 
+         
+end
