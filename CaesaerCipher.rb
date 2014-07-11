c7f39fef42870c180be1c44118ffe5b585da0779
Germain Boue <boue.germain@gmail.com>
2014-07-11 10:10:15 -0700

    The trick in order to accomodate all the edge cases involved extending the alphabet
    	- In fact, if you extend it then if it reaches a case where the num leads to an index that exceeds the alphabet it starts over again (to a certain pt)
    	- So I had to double the size of the alphabet array

--- CaesaerCipher.rb
+++ CaesaerCipher.rb
@@ -1,15 +1,15 @@
 def CaesarCipher(str,num)
-	alphabet = ('a'..'z').to_a
-	original = str.downcase.split("")
+	alphabet = ('a'..'z').to_a + ('a'..'z').to_a + ('A'..'Z').to_a + ('A'..'Z').to_a
+	original = str.split("")
 	adjusted = []
 	original.each do |letter|
-		if alphabet.include? (letter)
+		if alphabet.include? (letter) 
 			adjusted << alphabet[alphabet.index(letter) + num]
 		else 
 			adjusted << letter
 		end
 	end
-	p adjusted
+	# p adjusted
 	adjusted.join("")
 	#define a an array that holds all letters of alphabet 
 	#downcase string 
@@ -17,6 +17,7 @@ def CaesarCipher(str,num)
 	#if not a letter skip to next 
 end
 
-p CaesarCipher("ABC", 1)
-p CaesarCipher("Hello", 4)
-p CaesarCipher("aPPlication", 2)
+# p CaesarCipher("ABC", 1)
+# p CaesarCipher("Hello", 4)
+# p CaesarCipher("aPPlication", 2)
+p CaesarCipher("dogs", 8)
