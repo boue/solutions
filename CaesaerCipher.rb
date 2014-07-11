b41e558cbf31c02e92d8cea4b1ccdc9ec7dc7a5b
Germain Boue <boue.germain@gmail.com>
2014-07-11 08:53:46 -0700

    Pseudocde

---
+++ CaesaerCipher.rb
@@ -0,0 +1,6 @@
+def CaesarCipher(str,num)
+	#define a an array that holds all letters of alphabet 
+	#downcase string 
+	#if a letter then take index and move 2 up
+	#if not a letter skip to next 
+end
