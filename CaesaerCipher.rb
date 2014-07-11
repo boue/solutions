281f2925e5cd1bf3e44f54e41e1d008386f86ae7
Germain Boue <boue.germain@gmail.com>
2014-07-11 09:05:43 -0700

    Implementation without the looping
    	- takes the string downcases and splits it in order to manipulate array type
    	- idea is to loop through it with the index information
    	- then return the "original" yet we will need to change that to a string first

--- CaesaerCipher.rb
+++ CaesaerCipher.rb
@@ -1,6 +1,16 @@
 def CaesarCipher(str,num)
+	alphabet = ('a'..'z').to_a
+	original = str.downcase.split("")
+	original.each_with_index do |letter, index|
+		if alphabet.include? (letter)
+			
+		end
+	end
+	return original
 	#define a an array that holds all letters of alphabet 
 	#downcase string 
 	#if a letter then take index and move 2 up
 	#if not a letter skip to next 
 end
+
+p CaesarCipher("ABC", 1)
