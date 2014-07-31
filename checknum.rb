a3c7acb42026c74560c6c1df3ecb4c8973b4dcce
Germain Boue <boue.germain@gmail.com>
2014-07-30 21:20:12 -0700

    Finished

--- checknum.rb
+++ checknum.rb
@@ -1,3 +1,18 @@
-def CheckNum(num)
-
+def CheckNum(num1, num2)
+	if num1 == num2
+		"-1"
+	elsif num2 > num1
+		true
+	else
+		false
+	end
 end
+
+def assert_equals actual, expected
+  puts actual == expected || raise("expected #{expected} but got #{actual}")
+end
+
+assert_equals CheckNum(1, 1), "-1"
+assert_equals CheckNum(1, 2), true
+assert_equals CheckNum(1, -1), false
+
