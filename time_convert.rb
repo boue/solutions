48cf44b27d5f86a69f015085a12e20680c1dcd7f
Germain Boue <boue.germain@gmail.com>
2014-07-30 17:39:11 -0700

    Made few changes, better output

--- time_convert.rb
+++ time_convert.rb
@@ -1,7 +1,14 @@
 def TimeConvert(num)
-	mins = num % 60
-	hours = num / 60
-	return "#{mins}:#{hours}" 
+	mins = num % 60       # => 3, 6, 45
+	hours = num / 60      # => 1, 2, 0
+	"#{hours}:#{mins}"   # => "1:3:", "2:6:", "0:45:"
 end
 
-p TimeConvert(63)
+p TimeConvert(63)   # => "1:3:"
+p TimeConvert(126)  # => "2:6:"
+p TimeConvert(45)   # => "0:45:"
+
+
+# >> "1:3:"
+# >> "2:6:"
+# >> "0:45:"
