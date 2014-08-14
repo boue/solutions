e80dcad49b348ee14d04a76e93eec5c52c779a99
Germain Boue <boue.germain@gmail.com>
2014-08-14 14:38:47 -0500

    Have a result in 4 decimal places
    	- converted both nums into float integers
    	- made sure to round to 4

--- formatted_division.rb
+++ formatted_division.rb
@@ -1,6 +1,11 @@
 #input: two numbers
 #output: formatted commas and 4 digits after decimal
 def formatted_division(num1, num2)
-
+	num = num1.to_f / num2.to_f
+	num.round(4)
 
 end
+
+p formatted_division(2,4.5)
+p formatted_division(2.2,4.5)
+p formatted_division(1,231.342342)
