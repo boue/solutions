f24ed972e4f3b9e15bacd9fc01a3da6dd853be62
Germain Boue <boue.germain@gmail.com>
2014-08-14 15:05:07 -0500

    completed exercise wanted a string

--- formatted_division.rb
+++ formatted_division.rb
@@ -2,10 +2,10 @@
 #output: formatted commas and 4 digits after decimal
 def formatted_division(num1, num2)
 	num = (num1.to_f / num2.to_f).round(4)
-	#match the last 4 digits 
 	ending_trail = /([.]\d{4})/
 	rounded_part = num.to_s.match(ending_trail)[0]
-	comma_numbers(num).concat(rounded_part)
+	formatted_num = comma_numbers(num).concat(rounded_part)
+	formatted_num
 end
 
 def comma_numbers(number)
