45a2f7dd5bdddf53625f356e6488a77e2a421d58
Germain Boue <boue.germain@gmail.com>
2014-08-17 13:39:55 -0500

    Changed ceil to round as edge case tested showed wrong result

--- number_search.rb
+++ number_search.rb
@@ -2,7 +2,7 @@ def number_search(str)
 	array_of_num = str.scan(/\d/).join('').split("").collect { |n| n.to_i }
 	total = add_numbers(array_of_num)
 	total_num_letters = strip_numbers(str)
-	p (total/total_num_letters).ceil
+	p (total/total_num_letters).round
 end
 
 def add_numbers(arr)
@@ -15,3 +15,4 @@ def strip_numbers(str)
 end
 
 number_search("Hello6 9World 2, Nic8e D7ay!")
+number_search("One Number*1*")
