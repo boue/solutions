204c7d564882a5206b7de0e81fb2b7d952950f72
Germain Boue <boue.germain@gmail.com>
2014-08-17 13:36:13 -0500

    Created two seperate methods in order to do two tasks
    	- Had to convert to float to be able to get the ceil value of integer
    	- Stripping numbers involved two regular expressions, one which gets rid of anything but letters and the other that counts them
    	- Finally we are able to divide and ceil up

--- number_search.rb
+++ number_search.rb
@@ -1,12 +1,17 @@
 def number_search(str)
-	
 	array_of_num = str.scan(/\d/).join('').split("").collect { |n| n.to_i }
 	total = add_numbers(array_of_num)
-	total / total_num_letters
+	total_num_letters = strip_numbers(str)
+	p (total/total_num_letters).ceil
 end
 
 def add_numbers(arr)
-	arr.inject(:+)
+	arr.inject(:+).to_f
+end
+
+def strip_numbers(str)
+	str.gsub!(/\d\s?/, "")
+	str.scan(/[a-zA-z]/).count.to_f
 end
 
 number_search("Hello6 9World 2, Nic8e D7ay!")
