3c2f7316d702a9fe7d25b8c745031a1ebd6ab23e
Germain Boue <boue.germain@gmail.com>
2014-08-17 13:07:59 -0500

    pseudocode, two methods

---
+++ number_search.rb
@@ -0,0 +1,14 @@
+number_search(str)
+	
+	total_num_letters = 
+	array_of_num = 
+	total = add_numbers(array_of_num)
+	total / total_num_letters
+end
+
+add_numbers(arr)
+	sum = 0
+	arr.each do |n|
+		sum = sum + n 
+	end
+end
