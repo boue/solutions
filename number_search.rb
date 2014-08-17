53e32c1c9c51c78941706e9bb5ee17b9731e3334
Germain Boue <boue.germain@gmail.com>
2014-08-17 13:21:05 -0500

    Made array of numbers now need to count letters and store in a variable

--- number_search.rb
+++ number_search.rb
@@ -1,14 +1,12 @@
-number_search(str)
+def number_search(str)
 	
-	total_num_letters = 
-	array_of_num = 
+	array_of_num = str.scan(/\d/).join('').split("").collect { |n| n.to_i }
 	total = add_numbers(array_of_num)
 	total / total_num_letters
 end
 
-add_numbers(arr)
-	sum = 0
-	arr.each do |n|
-		sum = sum + n 
-	end
+def add_numbers(arr)
+	arr.inject(:+)
 end
+
+number_search("Hello6 9World 2, Nic8e D7ay!")
